0652de154bd0c4610c7088dd89dfd9150c44201e
AURELIEN LECOQ <aurelien.lecoq@free.fr>
2017-12-20 10:27:08 +0100

    tt

---
+++ resources/views/resultat.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+@section('title', 'Résultat')
+@section('content')
+            @foreach($response as $key => $value)
+         <article>
+             <span>Pays: {{ $value['name'] }}</span>
+             <span>Capitale: {{ $value['capital'] }}</span>
+             <span>Population: {{ $value['population'] }}</span>
+             <span>Continent: {{ $value['region'] }}</span>
+             <span>Monnaie: {{ $value['currencies'][0]['name'] }}</span>
+             <span>Indicatif téléphonique: +{{ $value['callingCodes'][0] }}</span>
+             <span>Langue: {{ $value['languages'][0]['nativeName'] }}</span>
+             <span>Latitude: {{ $value['latlng'][0] }}</span>
+             <span>Latitude: {{ $value['latlng'][1] }}</span>
+         </article>
+         @endforeach
+
+
+@endsection
