ce5a1f49169089d39daa56bbcf9eb8bb8712d5f6
Valentin <valentinlaidet1@gmail.com>
2017-12-20 11:05:18 +0100

    css age resultat

--- resources/views/resultat.blade.php
+++ resources/views/resultat.blade.php
@@ -1,18 +1,33 @@
 @extends('layout')
 @section('title', 'Résultat')
 @section('content')
+
        @foreach($response as $key => $value)
-      	<article>
-      		<span>Pays: {{ $value['name'] }}</span>
-      		<span>Capitale: {{ $value['capital'] }}</span>
-      		<span>Population: {{ $value['population'] }}</span>
-      		<span>Continent: {{ $value['region'] }}</span>
-      		<span>Monnaie: {{ $value['currencies'][0]['name'] }}</span>
-      		<span>Indicatif téléphonique: +{{ $value['callingCodes'][0] }}</span>
-      		<span>Langue: {{ $value['languages'][0]['nativeName'] }}</span>
-      		<span>Latitude: {{ $value['latlng'][0] }}</span>
-      		<span>Latitude: {{ $value['latlng'][1] }}</span>
-      	</article>
+
+      <div class="container-fluid">
+
+         <div class="row text-center vertical-center">
+
+           <div class="col-md-6">
+             
+              <article>
+                <h1>Résultat de votre recherche "{{ $search }}"</h1>
+                <span>Pays: {{ $value['name'] }}</span><br>
+                <span>Capitale: {{ $value['capital'] }}</span><br>
+                <span>Population: {{ $value['population'] }}</span><br>
+                <span>Continent: {{ $value['region'] }}</span><br>
+                <span>Monnaie: {{ $value['currencies'][0]['name'] }}</span><br>
+                <span>Indicatif téléphonique: +{{ $value['callingCodes'][0] }}</span><br>
+                <span>Langue: {{ $value['languages'][0]['nativeName'] }}</span><br>
+                <span>Latitude: {{ $value['latlng'][0] }}</span><br>
+                <span>Longitude: {{ $value['latlng'][1] }}</span><br>
+              </article>
+
+           </div>
+
+         </div>
+
+      </div>
 
        @endforeach
 @endsection
